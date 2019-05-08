630f3536be5380888dd0f394bb797021a655d148
Wout Slakhorst <wout.slakhorst@nedap.com>
2019-05-08 16:20:09 +0200

    Removed unused copy-paste code
    Signed-off-by: Wout Slakhorst <wout.slakhorst@nedap.com>

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -35,15 +35,7 @@ func NewCryptoBackend() (Backend, error) {
 // shared function to convert bytes to a RSA private key
 func bytesToPrivateKey(priv []byte) (*rsa.PrivateKey, error) {
 	block, _ := pem.Decode(priv)
-	enc := x509.IsEncryptedPEMBlock(block)
 	b := block.Bytes
-	var err error
-	if enc {
-		b, err = x509.DecryptPEMBlock(block, nil)
-		if err != nil {
-			return nil, err
-		}
-	}
 	key, err := x509.ParsePKCS1PrivateKey(b)
 	if err != nil {
 		return nil, err
